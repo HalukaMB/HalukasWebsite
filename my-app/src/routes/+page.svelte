43769b90addc15ef6a06e5aeb0a23ef7acf45d35
HalukaMB <mail@maier-borst.de>
2024-04-15 11:25:47 +0200

    starting from the regular svelte example app flavour

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		This is my overdue update of my own webpage.
+	</h1>
+
+	<Counter />
+</section>
+<section class="introduction">
+	<div class="introduction">Here you can roam around and get a rough idea of...</div>
+	<div class="worklink">my actual work</div>
+	<div class="bioLink">my bio</div>
+	<div class="codinglink">my attempts to improve my coding</div>
+	<div class="hobbylink">and my neck for odd hobbies</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
