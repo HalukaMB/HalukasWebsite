34ad7c41e615541412d7a4e56c03b7e5b85a51fc
HalukaMB <mail@maier-borst.de>
2024-04-15 21:38:03 +0200

    started adding picture

--- my-app/src/routes/about/+page.svelte
+++ my-app/src/routes/about/+page.svelte
@@ -6,16 +6,19 @@
 <div class="text-column">
 	<h1>About me</h1>
 
-	<div>
+	<div class="paragraph">
 		My name is Haluka Maier-Borst. 
-		I am a bit of a dev. I am a little bit more of a data scientist. 
+		I am a bit of a dev. I am a little  more of a data scientist. 
 		And I am definitely a curious, know-it-all, but still capable of self-doubt journalist. 
 	</div>
-	<div>
+	<picture>
+		<img id="profileHaluka" src="src/lib/images/haluka.png" alt="Haluka" />
+	</picture>
+	<div class="paragraph">
 		Currently, I am the team lead of the 
-		<span>data, interactive and visual team</span>
-		at a German business newspaper Handelsblatt.
-		Or short, we are the <span>&lt;div&gt;</span>-team.
+		<span class="highlight">data, interactive and visual team</span>
+		at a German business newspaper called Handelsblatt.
+		Or short, we are the <span class="highlight">&lt;div&gt;</span>-team.
 	</div>
 
 	<pre>npm create svelte@latest</pre>
